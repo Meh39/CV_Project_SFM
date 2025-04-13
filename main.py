7fb2da6f5c98f6230192fc2e16559a35841fb05c
Meh39 <117830253+Meh39@users.noreply.github.com>
2025-04-13 08:40:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sfm_pipeline import run_sfm_pipeline
 
-# Display a sample image for verification
 sample_img_path = "coil-100/coil-100/obj21__10.png"
 sample_img = cv2.imread(sample_img_path)
 if sample_img is not None:
@@ -15,18 +14,15 @@ if sample_img is not None:
 else:
     print("Sample image not found!")
 
-# Set the glob pattern for Object 21 in the COIL-100 dataset
 coil_pattern = "coil-100/coil-100/obj21__*.png"
 
-# Run the SfM pipeline
 point_cloud, global_R, global_t, used_frame_indices = run_sfm_pipeline(
     coil_pattern,
-    use_orb=True,         # Set True to use ORB features; set to False to use SIFT features
+    use_orb=True,
     interpolate=True,
     filter_outliers=True
 )
 
-# Save results locally
 np.save("point_cloud.npy", point_cloud)
 np.save("camera_rotations.npy", np.array(global_R))
 np.save("camera_translations.npy", np.array(global_t))
